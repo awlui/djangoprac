1eb49e053b1b7547987c8be91b5c6365ab4fc6c9
Andy Lui <awlui2014@gmail.com>
2018-02-02 22:04:22 -0800

    initial commit

---
+++ lists/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import include, url
+from django.contrib import admin
+
+urlpatterns = [
+    # Examples:
+    url(r'^new$', 'lists.views.new_list', name='new_list'),
+    url(r'^(\d+)/$', 'lists.views.view_list', name='view_list'),
+    url(r'^(\d+)/add_item$', 'lists.views.add_item', name='add_item'),
+]
