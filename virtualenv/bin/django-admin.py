0b88c812b4b6644a2c4c0b2b80c263ecdbb39e91
Andy Lui <awlui2014@gmail.com>
2018-02-04 09:11:07 -0800

    test

--- virtualenv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/andylui/PyProjects/book-practice/superlists/virtualenv/bin/python3.6
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
